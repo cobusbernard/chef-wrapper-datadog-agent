210758e586df43b00dc147ba8c93c7e856ead6d8
Cobus Bernard <cobusbernard@gmail.com>
2016-01-28 20:46:35 +0200

    Adding the Mesos configs.

--- attributes/mesos.rb
+++ attributes/mesos.rb
@@ -1,23 +1,21 @@
 if node.role?('mesos-master')
-  default['datadog']['mesos'] =
-    instances: [{
-      'url'               => 'http://localhost:5050',
-      'timeout' => 8,
-      'tags'              => [node.chef_environment]
-    }],
-    init_config: {
+  default['datadog']['mesos']["instances"] = [{
+    'url'               => 'http://localhost:5050',
+    'timeout' => 8,
+    'tags'              => [node.chef_environment]
+  }]
+  default['datadog']['mesos']["init_config"] = {
       default_timeout: 10
     }
 end
 
 if node.role?('mesos-slave')
-  default['datadog']['mesos'] =
-    instances: [{
-      'url'               => 'http://localhost:5051',
-      'timeout' => 8,
-      'tags'              => [node.chef_environment]
-    }],
-    init_config: {
+  default['datadog']['mesos']["instances"] = [{
+    'url'               => 'http://localhost:5051',
+    'timeout' => 8,
+    'tags'              => [node.chef_environment]
+  }]
+  default['datadog']['mesos']["init_config"] = {
       default_timeout: 10
     }
 end
